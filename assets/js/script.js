0eb5a838c6dc36b08e3df98b3990c5f93d0fc6a8
hope87210 <hope87210@gmail.com>
2019-04-12 14:52:09 +0800

    110619014-2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,29 @@
 import $ from'jquery';
-import 'bootstrap';
+
 import bird from '../images/bird.jpg';
-import squirrel from '../images/squirrel.jpg'
+import squirrel from '../images/squirrel.jpg';
+
+import "../css/normalize.css";
+import "../css/style.css";
+
+import "../css/fonts/PT-Sans-Bold.eot";
+import "../css/fonts/PT-Sans-Bold.svg";
+import "../css/fonts/PT-Sans-Bold.ttf";
+import "../css/fonts/PT-Sans-Bold.woff";
+import "../css/fonts/PT-Sans-BoldItalic.eot";
+import "../css/fonts/PT-Sans-BoldItalic.svg";
+import "../css/fonts/PT-Sans-BoldItalic.ttf";
+import "../css/fonts/PT-Sans-BoldItalic.woff";
+import "../css/fonts/PT-Sans-Italic.eot";
+import "../css/fonts/PT-Sans-Italic.svg";
+import "../css/fonts/PT-Sans-Italic.ttf";
+import "../css/fonts/PT-Sans-Italic.woff";
+import "../css/fonts/PT-Sans.eot";
+import "../css/fonts/PT-Sans.svg";
+import "../css/fonts/PT-Sans.ttf";
+import "../css/fonts/PT-Sans.woff";
+
+
 $('#image__bird').attr("src",bird);
 $('#image__squirrel').attr("src",squirrel);
 
